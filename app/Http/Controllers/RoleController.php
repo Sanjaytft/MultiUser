1f950ba3a437408a67bccd95109274576b9a1fde
Sanjaytft <sanjay.teamtft@gmail.com>
2024-06-22 23:11:29 +0545

    Controller for Roles and Permission

---
+++ app/Http/Controllers/RoleController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+
+class RoleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $roles = Role::get();
+        return view ('roleView.role.index', [
+            'roles' => $roles
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view ('roleView.role.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'string','unique:roles,name']
+        ]);
+        Role::create ([
+            'name' => $request->name
+        ]);
+
+        return redirect('roles')->with('status', 'Role created Sucessfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Role $role)
+    {
+
+        return view ('roleView.role.edit', [
+            //array or a compact function 
+            'role' => $role
+        ]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Role $role)
+    {
+        $request->validate([
+            'name' => ['required', 'string','unique:Roles,name,' 
+            .$role->id]
+        ]);
+        $role->update ([
+            'name' => $request->name
+        ]);
+
+        return redirect('roles')->with('status', 'Role Updated Sucessfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($roleId)
+    {
+        $role = Role::find($roleId);
+        $role->delete();
+        return redirect('roles')->with('status', 'Role Deleted Sucessfully');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,20 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('roleView.permission.create');
 });
+
+// While using resource request no array will come 
+Route::resource('permissions', App\Http\Controllers\PermissionController::class);
+
+// While using resource request no array will come 
+Route::get('permissions/{permissionId}/delete', [App\Http\Controllers\PermissionController::class, 'destroy']);
+
+// While using resource request no array will come 
+Route::resource('roles', App\Http\Controllers\RoleController::class);
+
+// While using resource request no array will come 
+Route::get('roles/{roleId}/delete', [App\Http\Controllers\RoleController::class, 'destroy']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
